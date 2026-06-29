7e000e0b1abf49857675b1643aa70dd2badaa855
shwetamishra25 <shwetamish2007@gmail.com>
2026-06-29 11:54:10 -0700

    Add files via upload

---
+++ Day 08/Q30.py
@@ -0,0 +1,7 @@
+# Number Triangle
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
---
+++ Day 08/Q32.py
@@ -0,0 +1,6 @@
+# Repeated Number Pattern
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print(str(i) * i)
